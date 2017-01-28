d5e0397512f13221bf50bef264cd6c96344881cc
Nimo <nimo.jser@gmail.com>
2017-01-28 13:48:46 +0800

    r-checkbox-checkbox => r-checkbox-input

--- __test__/basic.test.js
+++ __test__/basic.test.js
@@ -5,7 +5,7 @@ import { shallow, mount, render } from "enzyme"
 it('props.prefixClassName', function () {
     var app = mount(<Checkbox prefixClassName="m-check" />)
     expect(app.find('.m-check').length).toEqual(1)
-    expect(app.find('.m-check-checkbox').length).toEqual(1)
+    expect(app.find('.m-check-input').length).toEqual(1)
 })
 it('props.className', function () {
     var app = mount(<Checkbox className="myclass" />)
@@ -16,38 +16,38 @@ it('props.onChange', function () {
     var app = mount(<Checkbox checked={checked} onChange={function(e) {
         checked = e.target.checked
     }} />)
-    app.find('.r-checkbox-checkbox').simulate('change', {target:{checked: true}})
+    app.find('.r-checkbox-input').simulate('change', {target:{checked: true}})
     expect(checked).toEqual(true)
-    app.find('.r-checkbox-checkbox').simulate('change', {target:{checked: false}})
+    app.find('.r-checkbox-input').simulate('change', {target:{checked: false}})
     expect(checked).toEqual(false)
 })
 it('props.checked', function () {
     var app = mount(<Checkbox checked={true} />)
     expect(app.find('.r-checkbox').hasClass('r-checkbox--checked')).toEqual(true)
-    expect(app.find('.r-checkbox-checkbox').prop('checked')).toEqual(true)
+    expect(app.find('.r-checkbox-input').prop('checked')).toEqual(true)
     var app = mount(<Checkbox />)
     expect(app.find('.r-checkbox').hasClass('r-checkbox--checked')).toEqual(false)
-    expect(app.find('.r-checkbox-checkbox').prop('checked')).toEqual(false)
+    expect(app.find('.r-checkbox-input').prop('checked')).toEqual(false)
 })
 it('props.name', function () {
     var app = mount(<Checkbox name="abc" />)
-    expect(app.find('.r-checkbox-checkbox').prop('name')).toEqual('abc')
+    expect(app.find('.r-checkbox-input').prop('name')).toEqual('abc')
     var app = mount(<Checkbox  />)
-    expect(app.find('.r-checkbox-checkbox').prop('name')).toEqual('')
+    expect(app.find('.r-checkbox-input').prop('name')).toEqual('')
 })
 
 it('props.disabled', function () {
     var app = mount(<Checkbox disabled={true} />)
     expect(app.find('.r-checkbox').hasClass('r-checkbox--disabled')).toEqual(true)
-    expect(app.find('.r-checkbox-checkbox').prop('disabled')).toEqual(true)
+    expect(app.find('.r-checkbox-input').prop('disabled')).toEqual(true)
     var app = mount(<Checkbox />)
     expect(app.find('.r-checkbox').hasClass('r-checkbox--disabled')).toEqual(false)
-    expect(app.find('.r-checkbox-checkbox').prop('disabled')).toEqual(false)
+    expect(app.find('.r-checkbox-input').prop('disabled')).toEqual(false)
 })
 
 it('props.type', function () {
     var app = mount(<Checkbox />)
-    expect(app.find('.r-checkbox-checkbox').prop('type')).toEqual('checkbox')
+    expect(app.find('.r-checkbox-input').prop('type')).toEqual('checkbox')
     var app = mount(<Checkbox type="radio" />)
-    expect(app.find('.r-checkbox-checkbox').prop('type')).toEqual('radio')
+    expect(app.find('.r-checkbox-input').prop('type')).toEqual('radio')
 })
